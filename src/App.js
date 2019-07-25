2667f533691c8738880cb36e0ed1eacdb941fd73
Clayton Wang <claydwang@gmail.com>
2019-07-25 10:12:39 -0700

    create Calender Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calender from './Components/calender';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: '',
+    appointments: []
+  };
+  render() {
+    return (
+      <>
+        <h1>Clay's Appointment App</h1>
+        <Calender />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/calender.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Calender extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <h1>Calender</h1>
+      </>
+    );
+  }
+}
+
+export default Calender;
