2357e962c52dd1a84ceae494217e6fbfaa136b58
Clayton Wang <claydwang@gmail.com>
2019-08-06 14:32:04 -0700

    add users routes and controllers

--- Controllers/users.js
+++ Controllers/users.js
@@ -1,1 +1,47 @@
 const User = require('../Models/User');
+const signToken = require('../serverAuth.js').signToken;
+
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const allUsers = await User.find({});
+      res.json({
+        users: allUsers,
+        message: success
+      });
+    } catch (err) {
+      console.log(err);
+      res.json(err);
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const newUser = await User.create(req.body);
+      res.json({
+        user: newUser,
+        success: true
+      });
+    } catch (err) {
+      console.log(err);
+      res.json(err);
+    }
+  },
+  profile: async (req, res) => {},
+  // the login route
+  authenticate: async (req, res) => {
+    // check if the user exists
+    try {
+      const loggedUser = await User.findOne({ email: req.body.email });
+      // if there's no user or the password is invalid
+      if (!loggedUser || !loggedUser.validPassword(req.body.password)) {
+        // deny access
+        return res.json({ success: false, message: 'Invalid credentials.' });
+      }
+      const token = signToken(user);
+      res.json({ success: true, message: 'Token attached.', token });
+    } catch (err) {
+      console.log(err);
+      res.json(err);
+    }
+  }
+};
--- Routes/users.js
+++ Routes/users.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../Controllers/users');
+
+router.get('/', userController.index);
+router.get('/profile', userController.profile);
+router.post('/new', userController.create);
+router.post('/', userController.authenticate);
