e65d8d0af4b3201811e318ed6c9370845bda7e2e
Clayton Wang <claydwang@gmail.com>
2019-08-14 11:31:51 -0700

    finish card layout

--- client/src/Components/UserCard.js
+++ client/src/Components/UserCard.js
@@ -4,16 +4,18 @@ const UserCard = ({ user }) => {
   const { image, email, company, name, title } = user;
   return (
     <div className="UserC">
-      <div className="UserC__img-wrapper">
-        <img src={image} alt="profile" className="UserC__image" />
-      </div>
-      <div className="UserC__info-wrapper">
-        <h1 className="UserC__name">{name}</h1>
-        <h2 className="UserC__company">{company}</h2>
-        <p className="UserC__title">{title}</p>
-        <h3 className="UserC__contact">Contact:</h3>
-        <p className="UserC__email">{email}</p>
-        <button className="UserC__book-btn">Book</button>
+      <div className="UserC__wrapper">
+        <div className="UserC__img-wrapper">
+          <img src={image} alt="profile" className="UserC__image" />
+        </div>
+        <div className="UserC__info-wrapper">
+          <h1 className="UserC__name">{name}</h1>
+          <h2 className="UserC__company">{company}</h2>
+          <p className="UserC__title">{title}</p>
+          <h3 className="UserC__contact">Contact:</h3>
+          <p className="UserC__email">{email}</p>
+          <button className="UserC__book-btn">Book</button>
+        </div>
       </div>
     </div>
   );
--- client/src/Layouts/Splash.js
+++ client/src/Layouts/Splash.js
@@ -6,12 +6,12 @@ import SignUp from '../Components/SignUp';
 const Splash = ({ clicked, click, user, setUser }) => {
   return (
     <div className="Splash">
+      {(clicked || user) && <Redirect to="/users" />}
       <div className="Splash__container">
         <h1 className="Splash__title">Well Health</h1>
         <button className="Splash__button" onClick={click}>
           Book Now
         </button>
-        {(clicked || user) && <Redirect to="/form" />}
       </div>
       <Login setUser={setUser} />
       <SignUp setUser={setUser} />
